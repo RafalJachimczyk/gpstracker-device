ff7ba171561f3eafb03c1ac40cb7cd08e00d35d9
Rafal Jachimczyk <>
2020-09-19 10:03:25 +0200

    Extends watchdog timer

--- src/main.cpp
+++ src/main.cpp
@@ -50,7 +50,7 @@ float current = 0.0;
 Position position {0,0};
 
 volatile int counter;      // Count number of times ISR is called.
-volatile int countmax = 3; // Arbitrarily selected 3 for this example.
+volatile int countmax = 8; // Arbitrarily selected 3 for this example.
                           // Timer expires after about 24 secs if
                           // 8 sec interval is selected below.
 
