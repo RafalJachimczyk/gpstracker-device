b420df59b00a26dd1b0eac6726daa569532c431f
Rafal Jachimczyk <rafal.jachimczyk@bbc.co.uk>
2021-09-12 09:30:15 +0200

    Adds credentials template

---
+++ src/credentials_template.h
@@ -0,0 +1,7 @@
+// WiFi credentials
+const char *ssid = "YOUR_SSID";
+const char *password = "YOUR_PASSWORD";
+// Your GPRS credentials, if any
+const char apn[]  = "three.co.uk";
+const char user[] = "";
+const char pass[] = "";
